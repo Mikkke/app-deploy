46f0ed523cf885ea5e1ec43d5677f2674a6a764e
Mikkke <jkellymichael@gmail.com>
2020-07-21 11:30:07 +0200

    Updates

---
+++ precache-manifest.2821937048c7f5c3131b4f659afb91cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43c997af9e6f73cd5d7d6daea0d9428",
+    "url": "/app-deploy/index.html"
+  },
+  {
+    "revision": "a03116239b428550e57e",
+    "url": "/app-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "495bcbf6d77548823a92",
+    "url": "/app-deploy/static/js/2.325fad93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-deploy/static/js/2.325fad93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03116239b428550e57e",
+    "url": "/app-deploy/static/js/main.7422b252.chunk.js"
+  },
+  {
+    "revision": "39afe1956ee41cb389fd",
+    "url": "/app-deploy/static/js/runtime-main.7857879e.js"
+  }
+]);
